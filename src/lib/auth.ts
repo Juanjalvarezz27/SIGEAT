cffa1481a602be1fbf1f2e7a7658cb52464758b3
Juanjalvarezz27 <jjsalvarezz@gmail.com>
2026-01-14 14:30:38 -0400

    Prueba Session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -42,7 +42,7 @@ export const authOptions: NextAuthOptions = {
   ],
   session: {
     strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 días
+    maxAge: 60,
   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
