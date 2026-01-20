97c2b3859372278176d8898abd78ee3cd2e26ca6
Juanjalvarezz27 <jjsalvarezz@gmail.com>
2026-01-20 18:40:29 -0400

    Fix bg

--- src/app/home/monedero/page.tsx
+++ src/app/home/monedero/page.tsx
@@ -123,7 +123,7 @@ export default function MonederoPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 pb-20">
+    <div className="min-h-screen pb-20">
       <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
 
         {/* Header principal */}
